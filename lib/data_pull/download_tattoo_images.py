332523be37c7059a38b79f7a1a97b4ba7006d648
harshbaberwal21 <harshbaberwal2k19@gmail.com>
2024-09-21 15:26:31 -0500

    format downmload module code

--- lib/data_pull/download_tattoo_images.py
+++ lib/data_pull/download_tattoo_images.py
@@ -1,7 +1,9 @@
 """Functions to download the tattoo images."""
+
 import os
 import time
 import requests
+
 
 def download_tattoo_images(tattoo_meta_data_processed, download_path):
     """Download the tattoo images for tattoos in processed metadata using
@@ -19,35 +21,40 @@ def download_tattoo_images(tattoo_meta_data_processed, download_path):
     if os.path.exists(download_path):
         download_folder_path = os.path.join(download_path, "raw_tattoo_images")
     else:
-        raise ValueError("Given download path either does not exist or is invalid.\n\
-            Please enter a valid path that exists.")
+        raise ValueError(
+            "Given download path either does not exist or is invalid.\n\
+            Please enter a valid path that exists."
+        )
 
     if not os.path.exists(download_folder_path):
         os.mkdir(download_folder_path)
 
     download_counter = 0
-    tattoo_urls_w_ids  = tattoo_meta_data_processed[['tattoo_id', 'image_url']].drop_duplicates()
+    tattoo_urls_w_ids = tattoo_meta_data_processed[
+        ["tattoo_id", "image_url"]
+    ].drop_duplicates()
 
-    for tattoo_url_row in tattoo_urls_w_ids.sort_values('tattoo_id').iterrows():
-        image_url = tattoo_url_row[1]['image_url']
-        tattoo_id = tattoo_url_row[1]['tattoo_id']
+    for tattoo_url_row in tattoo_urls_w_ids.sort_values("tattoo_id").iterrows():
+        image_url = tattoo_url_row[1]["image_url"]
+        tattoo_id = tattoo_url_row[1]["tattoo_id"]
 
         try:
-            download_image(image_url, f'{download_folder_path}/{tattoo_id}')
+            download_image(image_url, f"{download_folder_path}/{tattoo_id}")
             download_counter += 1
         except Exception as e:
-            print('Last tattoo_id downloaded: ', tattoo_id)
+            print("Last tattoo_id downloaded: ", tattoo_id)
             raise e
 
-        if download_counter%10 == 0:
+        if download_counter % 10 == 0:
             time.sleep(2)
 
-        if download_counter%100 == 0:
+        if download_counter % 100 == 0:
             time.sleep(5)
 
-        if download_counter%1000 == 0:
-            print(f'Downloaded {download_counter} images')
+        if download_counter % 1000 == 0:
+            print(f"Downloaded {download_counter} images")
             time.sleep(10)
+
 
 def download_image(image_url, download_path_w_name):
     """Download the image from the given URL.
@@ -58,7 +65,7 @@ def download_image(image_url, download_path_w_name):
         in with filename included, except the extension.
     """
     img_data = requests.get(image_url, timeout=10).content
-    with open(f'{download_path_w_name}.jpg', 'wb') as handler:
+    with open(f"{download_path_w_name}.jpg", "wb") as handler:
         handler.write(img_data)
 
 
