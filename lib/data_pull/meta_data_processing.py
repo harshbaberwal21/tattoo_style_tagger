cab797e596d06f59c408f24f73c99102449f47b5
harshbaberwal21 <harshbaberwal2k19@gmail.com>
2024-09-21 11:07:32 -0500

    update formatting of metadata processing module

--- lib/data_pull/meta_data_processing.py
+++ lib/data_pull/meta_data_processing.py
@@ -1,10 +1,5 @@
-import os
-import requests
-import json
-import numpy as np
-import pandas as pd
-import time
-import argparse
+"""Functions to process the tattoos' meta data."""
+
 
 def process_tattoo_meta_data_to_long_form(tattoo_images_meta_data):
     """Process tattoos' meta data and return a dataframe.
@@ -21,30 +16,32 @@ def process_tattoo_meta_data_to_long_form(tattoo_images_meta_data):
 
     if "searched_style_query" in tattoo_images_meta_data:
         tattoo_images_meta_data.drop(columns="searched_style_query", inplace=True)
-    
-
 
     tattoo_images_meta_data.drop_duplicates(inplace=True)
 
-    tattoo_images_meta_data["styles"] = tattoo_images_meta_data[
-        "styles"].str.replace('[', "").str.replace(']', "").str.split(",")
+    tattoo_images_meta_data["styles"] = (
+        tattoo_images_meta_data["styles"]
+        .str.replace("[", "")
+        .str.replace("]", "")
+        .str.split(",")
+    )
 
     tattoo_meta_data_exploded = tattoo_images_meta_data.explode(column="styles")
-    tattoo_meta_data_exploded["styles"] = tattoo_meta_data_exploded['styles'].str.strip()
-    tattoo_meta_data_exploded["styles"] = tattoo_meta_data_exploded["styles"].str.replace("'", "")
+    tattoo_meta_data_exploded["styles"] = tattoo_meta_data_exploded[
+        "styles"
+    ].str.strip()
+    tattoo_meta_data_exploded["styles"] = tattoo_meta_data_exploded[
+        "styles"
+    ].str.replace("'", "")
 
     tattoo_meta_data_exploded = tattoo_meta_data_exploded.loc[
-        ~tattoo_meta_data_exploded["styles"].isin([""]),:
-        ].copy()
+        ~tattoo_meta_data_exploded["styles"].isin([""]), :
+    ].copy()
 
     return tattoo_meta_data_exploded
 
 
-
-
-def filter_out_tattoos(
-        tattoo_meta_data_exploded,
-        style_labels_count_threshold=7):
+def filter_out_tattoos(tattoo_meta_data_exploded, style_labels_count_threshold=7):
     """Filter out tattoos with style label counts
     greater than threshold. The default threshold is 7,
     meaning all images are tagged with at most 7 distinct styles.
@@ -62,16 +59,20 @@ def filter_out_tattoos(
     tattoo_meta_data_exploded = tattoo_meta_data_exploded.copy()
 
     tattoo_images_style_counts = tattoo_meta_data_exploded.groupby(
-        'image_url', as_index=False
-        )['styles'].nunique()
+        "image_url", as_index=False
+    )["styles"].nunique()
 
     tattoo_urls_to_consider = tattoo_images_style_counts[
-        tattoo_images_style_counts['styles']<=style_labels_count_threshold]['image_url'].values
+        tattoo_images_style_counts["styles"] <= style_labels_count_threshold
+    ]["image_url"].values
 
-
-    tattoo_meta_data_exploded_filtered = tattoo_meta_data_exploded[
-    tattoo_meta_data_exploded['image_url'].isin(tattoo_urls_to_consider)
-    ].reset_index(drop=True).copy()
+    tattoo_meta_data_exploded_filtered = (
+        tattoo_meta_data_exploded[
+            tattoo_meta_data_exploded["image_url"].isin(tattoo_urls_to_consider)
+        ]
+        .reset_index(drop=True)
+        .copy()
+    )
 
     return tattoo_meta_data_exploded_filtered
 
@@ -90,16 +91,22 @@ def handle_duplicates(tattoo_meta_data_exploded_filtered):
 
     # tattoo_meta_data_exploded_filtered.drop(columns='tattoo_id', inplace=True)
 
-    new_tattoo_ids = tattoo_meta_data_exploded_filtered[
-        ['image_url']
-        ].drop_duplicates().reset_index(drop=True).reset_index()
-    new_tattoo_ids.rename(columns={'index':'tattoo_id'}, inplace=True)
-    new_tattoo_ids['tattoo_id'] = new_tattoo_ids['tattoo_id'] + 1
+    new_tattoo_ids = (
+        tattoo_meta_data_exploded_filtered[["image_url"]]
+        .drop_duplicates()
+        .reset_index(drop=True)
+        .reset_index()
+    )
+    new_tattoo_ids.rename(columns={"index": "tattoo_id"}, inplace=True)
+    new_tattoo_ids["tattoo_id"] = new_tattoo_ids["tattoo_id"] + 1
 
-    tattoo_meta_data_exploded_filtered.rename(columns={'tattoo_id':'orig_tattoo_id'}, inplace=True)
+    tattoo_meta_data_exploded_filtered.rename(
+        columns={"tattoo_id": "orig_tattoo_id"}, inplace=True
+    )
 
     tattoo_meta_data_exploded_filtered_final = tattoo_meta_data_exploded_filtered.merge(
-    new_tattoo_ids, on='image_url', how='left')
+        new_tattoo_ids, on="image_url", how="left"
+    )
 
     # Null Check
     print(tattoo_meta_data_exploded_filtered_final.isna().sum())
