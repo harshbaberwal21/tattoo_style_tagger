514c62fff397680a697debcbc945cac09d651c1f
harshbaberwal21 <harshbaberwal2k19@gmail.com>
2024-09-23 18:41:26 -0500

    add initial code for modeling data prep

---
+++ lib/modeling/data_splitting.py
@@ -0,0 +1,82 @@
+import numpy as np
+
+def model_data_splitting(tattoos_meta_data_processed_augmented, val_perc=0.1, test_perc=0.1, dev=False):
+    """Split model data into training & test sets when run in prod, and training,  valiation & test sets
+    when run in dev. The function tries to maintain the split percentages for all data as well as
+    for a particular style.
+    Args:
+        tattoos_meta_data_processed_augmented (pd.core.frame.DataFrame): Processed tattoos metadata
+        with augmented data as well.
+        val_perc (float, optional): Validation set percentage. Defaults to 0.1.
+        test_perc (float, optional): Test set percentage. Defaults to 0.1.
+        dev (bool, optional): To indicate dev or prod run.
+
+    Returns:
+        pd.core.frame.DataFrame: Tattoos metadata with a column example_type to indicate
+        if the image is to be considered for train, validation or test
+    """
+    tattoos_meta_data_processed_augmented_filtered = tattoos_meta_data_processed_augmented[
+        tattoos_meta_data_processed_augmented['to_process']
+        ].copy()
+
+    syle_tattoo_counts = tattoos_meta_data_processed_augmented.groupby('styles', as_index=False)['tattoo_id'].nunique()
+
+    val_perc = test_perc = 0.1
+    np_repr = np.random.RandomState(21)
+
+    test_set_ids = set()
+    val_set_ids = set()
+    train_set_ids = set()
+
+    for row in syle_tattoo_counts.sort_values('tattoo_id', ascending=True).iterrows():
+        row_val = row[1]
+        style_name = row_val['styles']
+        tattoo_count_for_style = row_val['tattoo_id']
+
+        if dev:
+            val_set_size = int(np.ceil(tattoo_count_for_style * val_perc))
+        test_set_size = int(np.ceil(tattoo_count_for_style * test_perc))
+        
+        tattoo_image_data_for_style = tattoos_meta_data_processed_augmented_filtered[tattoos_meta_data_processed_augmented_filtered['styles']==style_name].copy()
+        tattoo_ids_for_style = tattoo_image_data_for_style[['tattoo_id']].drop_duplicates().reset_index(drop=True)
+        
+        if dev:
+            val_set_size_left = val_set_size - tattoo_ids_for_style[tattoo_ids_for_style['tattoo_id'].isin(val_set_ids)].shape[0]
+        test_set_size_left = test_set_size - tattoo_ids_for_style[tattoo_ids_for_style['tattoo_id'].isin(test_set_ids)].shape[0]
+        
+        tattoo_ids_for_style_left = tattoo_ids_for_style[
+        (~tattoo_ids_for_style['tattoo_id'].isin(val_set_ids)) &
+        (~tattoo_ids_for_style['tattoo_id'].isin(train_set_ids)) &
+        (~tattoo_ids_for_style['tattoo_id'].isin(test_set_ids))
+        ].copy()
+        
+        if dev:
+            style_val_ids = np_repr.choice(tattoo_ids_for_style_left['tattoo_id'].values, size = val_set_size_left, replace=False)
+            tattoo_ids_for_style_left_after_val = tattoo_ids_for_style_left[~tattoo_ids_for_style_left['tattoo_id'].isin(style_val_ids)]
+            style_test_ids = np_repr.choice(tattoo_ids_for_style_left_after_val['tattoo_id'].values, size = test_set_size_left, replace=False)
+        else:
+            style_test_ids = np_repr.choice(tattoo_ids_for_style_left['tattoo_id'].values, size = test_set_size_left, replace=False)
+
+        if dev:
+            val_set_ids = val_set_ids.union(set(style_val_ids))
+        test_set_ids = test_set_ids.union(set(style_test_ids))
+
+        if dev:
+            train_set_ids = train_set_ids.union(
+            set(tattoo_ids_for_style_left['tattoo_id'].values).difference(set(style_val_ids)).difference(set(style_test_ids))
+            )
+        else:
+            train_set_ids = train_set_ids.union(
+            set(tattoo_ids_for_style_left['tattoo_id'].values).difference(set(style_test_ids))
+            )
+
+    tattoos_meta_data_processed_augmented.loc[:,'example_type'] = 'train'
+    tattoos_meta_data_processed_augmented.loc[tattoos_meta_data_processed_augmented['tattoo_id'].isin(test_set_ids),'example_type'] = 'test'
+
+    if dev:
+        tattoos_meta_data_processed_augmented.loc[tattoos_meta_data_processed_augmented['tattoo_id'].isin(val_set_ids),'example_type'] = 'val'
+
+    return tattoos_meta_data_processed_augmented
+
+if __name__ == "__main__":
+    pass
---
+++ lib/modeling/dataset_loader.py
@@ -0,0 +1,109 @@
+"""Module containing functions for defining dataset and loading data."""
+
+import os
+from PIL import Image
+import torch
+import torch.nn.functional as F
+from torch.utils.data import Dataset, DataLoader
+from torchvision import transforms
+
+
+class TattooDataset(Dataset):
+    """Tattoo images dataset."""
+
+    def __init__(self, id_df, labels_df, label_index_map, root_dir, transform=None):
+        """
+        Arguments:
+            labels_df (string): Path to the csv with style labels.
+            root_dir (string): Directory with all the images.
+            transform (callable, optional): Optional transform to be applied
+                on a sample.
+        """
+        self.labels_df = labels_df
+        self.root_dir = root_dir
+        self.transform = transform
+        self.ids_df = id_df
+        self.label_index_map = label_index_map
+        self.len = len(self.ids_df)
+        
+    def __len__(self):
+        return self.len
+
+    def __getitem__(self, idx):
+        
+        tattoo_id = self.ids_df.loc[idx,'tattoo_id']
+        
+        img_name = os.path.join(self.root_dir, f"{tattoo_id}.jpg")
+        image = Image.open(img_name)
+
+        tattoo_style_positions = self.labels_df[self.labels_df['tattoo_id']==tattoo_id]['styles'].map(
+            lambda x: self.label_index_map[x]
+        ).values
+
+        labels = torch.zeros(len(self.label_index_map), dtype=torch.float32)
+        labels[tattoo_style_positions] = 1
+
+        
+        if self.transform:
+            transformed_image_tansor = self.transform(image)
+
+        return transformed_image_tansor, labels
+    
+
+def pad_to_square_transform(image_tensor):
+    """Pad an image to make it a square image. This is done to avoid
+    image distortion when the image is resized to fixed lower dimension.
+
+    Args:
+        image_tensor (tensor): Tensor of the image to be padded.
+
+    Returns:
+        tensor: padded image tensor.
+    """
+
+    # Calculate padding
+    original_height, original_width = image_tensor.shape[1], image_tensor.shape[2]
+    target_height = target_width = max(original_height, original_width)
+    
+    padding_left = (target_width - original_width) // 2
+    padding_top = (target_height - original_height) // 2
+    padding_right = target_width - original_width - padding_left
+    padding_bottom = target_height - original_height - padding_top
+    
+    # Apply padding
+    padded_image_tensor = F.pad(image_tensor, (padding_left, padding_right, padding_top, padding_bottom), "constant", 0)
+    
+    return padded_image_tensor
+
+
+def get_image_tranforms(target_image_dims = (256, 256)):
+    """Get data transforms to be applied to an image.
+
+    Args:
+        target_image_dims (tuple, optional): Target image dimensions. Defaults to (256, 256).
+
+    Returns:
+        data_transoform: transformer for image tensor.
+    """
+    image_tranforms = transforms.Compose([
+        transforms.ToTensor(),
+        pad_to_square_transform,
+        transforms.Resize(target_image_dims),
+    ])
+
+    return image_tranforms
+
+def get_data_loader(dataset, batch_size):
+    """Get data loader.
+
+    Args:
+        dataset (TattooDataset): An instance of TattooDataset
+        batch_size (int): batch size for model training.
+
+    Returns:
+        _type_: _description_
+    """
+
+    data_loader = DataLoader(dataset, batch_size=batch_size, shuffle=True)
+
+    return  data_loader
