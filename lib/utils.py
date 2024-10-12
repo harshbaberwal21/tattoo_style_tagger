385a9cd7c7ae88853c775294c2252bcfa66d75e7
harshbaberwal21 <harshbaberwal2k19@gmail.com>
2024-10-12 15:01:36 -0500

    add get_device function to utils

--- lib/utils.py
+++ lib/utils.py
@@ -1,5 +1,6 @@
 """Utility Functions"""
 import pandas as pd
+import torch
 
 def read_csv_data(path, **kwargs):
     """Read data from a CSV file
@@ -11,3 +12,12 @@ def read_csv_data(path, **kwargs):
         pd.core.frame.DataFrame: data loaded from csv
     """
     return pd.read_csv(path, **kwargs)
+
+
+def get_device():
+    if torch.cuda.is_available():
+        return "cuda"
+    elif torch.backends.mps.is_available():
+        return "mps"
+    else:
+        return "cpu"
